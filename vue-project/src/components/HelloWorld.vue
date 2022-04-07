aa9a324b2b2880a4d71cf417c2ed35107593837b
nhkiiim <nhkiiim@naver.com>
2022-04-07 11:42:12 +0900

    fix: modify schedule item

--- vue-project/src/components/HelloWorld.vue
+++ vue-project/src/components/HelloWorld.vue
@@ -55,27 +55,16 @@
       names: ['김나현님 재택', '김수람님 재택', '권은님 휴가', '허예슬님 공가', '안태영님 재택', '김유민님 재택', '정소윤님 재택'],
     }),
     methods: {
-      getEvents ({ start, end }) {
+      getEvents () {
         const events = []
 
-        const min = new Date(`${start.date}T00:00:00`)
-        const max = new Date(`${end.date}T23:59:59`)
-        const days = (max.getTime() - min.getTime()) / 86400000
-        const eventCount = this.rnd(days, days + 20)
-
-        for (let i = 0; i < eventCount; i++) {
-          const allDay = this.rnd(0, 3) === 0
-          const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-          const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-          const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-          const second = new Date(first.getTime() + secondTimestamp)
-
+        for (let i = 0; i < 4; i++) {
           events.push({
             name: this.names[this.rnd(0, this.names.length - 1)],
-            start: first,
-            end: second,
+            start: new Date('2022-04-05'),
+            end: new Date('2022-04-06'),
             color: this.colors[this.rnd(0, this.colors.length - 1)],
-            timed: !allDay,
+            // timed: !allDay,
           })
         }
 
